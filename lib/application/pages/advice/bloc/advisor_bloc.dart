17304f3cd9300825c87bdb21bc26074b79af6acd
Mohammed <mohammaddiari@hotmail.com>
2025-01-22 16:08:15 +0800

    Add equatable package

--- lib/application/pages/advice/bloc/advisor_bloc.dart
+++ lib/application/pages/advice/bloc/advisor_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -6,7 +7,10 @@ part 'advisor_state.dart';
 
 class AdvisorBloc extends Bloc<AdvisorEvent, AdvisorState> {
   AdvisorBloc() : super(AdvisorInitial()) {
-    on<AdviceRequestedEvent>((event, emit) async {
+    on<AdviceRequestedEvent>((
+      AdviceRequestedEvent event,
+      Emitter<AdvisorState> emit,
+    ) async {
       emit(AdvisorStateLoading());
       debugPrint('fake get advice triggered');
       await Future<void>.delayed(const Duration(seconds: 3));
--- lib/application/pages/advice/bloc/advisor_state.dart
+++ lib/application/pages/advice/bloc/advisor_state.dart
@@ -1,7 +1,10 @@
 part of 'advisor_bloc.dart';
 
 @immutable
-sealed class AdvisorState {}
+sealed class AdvisorState extends Equatable {
+  @override
+  List<Object?> get props => <Object?>[];
+}
 
 final class AdvisorInitial extends AdvisorState {}
 
@@ -10,9 +13,15 @@ final class AdvisorStateLoading extends AdvisorState {}
 final class AdvisorStateLoaded extends AdvisorState {
   AdvisorStateLoaded({required this.advice});
   final String advice;
+
+  @override
+  List<Object?> get props => <Object?>[advice];
 }
 
 final class AdvisorStateError extends AdvisorState {
   AdvisorStateError({required this.message});
   final String message;
+
+  @override
+  List<Object?> get props => <Object?>[message];
 }
